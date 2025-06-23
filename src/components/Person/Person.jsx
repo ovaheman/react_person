d6d0016653a1ca135a656bcde2b0f399fb330a9a
Orest Vaheman <ovaheman@gmail.com>
2025-06-23 23:57:51 +0300

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,30 @@
-// export const Person = ({ person }) => ();
+import cn from 'classnames';
+export const Person = ({ person: { name, age, isMarried, sex, partnerName } }) => (
+
+  <div className="App">
+  <section className="Person">
+    <h2 className="Person__name">My name is {name}</h2>
+      {age > 0 ? (
+        <p className='Person__age'>I am {age}</p>
+      ) :
+        null
+      }
+      {isMarried === false ? (
+        <p className='Person__partner'>I am not married</p>
+      ) :
+        null}
+      {whoIsPartner(sex, partnerName, isMarried)}
+  </section>
+</div>
+);
+
+const whoIsPartner = (sex, partnerName, isMarried) => {
+  if (sex === 'm' && isMarried === true) {
+    return <p className='Person__partner'>{partnerName} is my wife</p>
+  }
+
+  if (sex === 'f' && isMarried === true) {
+    return <p className='Person__partner'>{partnerName} is my husband</p>
+  }
+}
+
